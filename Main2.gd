aa7904df7f236fe7ad9f69d5fe9e28bd88372952
int-72h <int-10h@protonmail.com>
2022-08-12 23:02:58 +0100

    added level triggers, hopefully shouldn't break abilites

---
+++ Main2.gd
@@ -0,0 +1,21 @@
+extends Node2D
+const enemy_scene = preload("res://Scenes/Enemy.tscn")
+const enemy_spawn_array = [[enemy_scene,Vector2(128,192)],[enemy_scene,Vector2(100,200)]] # [[scene,positioning]...]
+var enemies = []
+func _process(delta):
+	$HUD.set_health($Player.get("health"))
+	$HUD.set_speed($Player.get("velocity").x)
+	$HUD.set_firearm($Player/CarriedItem.get_children()[0]) # do this on event not every frame
+	$HUD.set_consts($Player.get("consts"))
+	$HUD.set_ammo() # ditto
+	$HUD.set_debug($Player.get("debug_text"))
+
+func _ready():
+	#$Player/Abilities.call("init",[0,1])
+	for x in enemy_spawn_array:
+		var z = x[0].instance()
+		z.set("global_position",x[1])
+		z.connect("hurt", get_node("/root/root/Player"), "_on_Enemy_hurt")
+		#z.connect("hurt", z, "on_hit")
+		get_node("/root/root").add_child(z)
+		enemies.append(z)
---
+++ scripts/entity/LevelEndTrigger1.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var collision = get_overlapping_bodies()
+	if collision != []:
+		print(collision)
+		get_tree().change_scene("res://Scenes/Level2.tscn")
